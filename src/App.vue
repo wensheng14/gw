1894d6262cb9540c13816b8c9cc91b0ad606fc49
wensheng <1733115718@qq.com>
2020-12-27 13:27:20 +0800

    add carousel

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
   <div id="app">
     <logo></logo>
     <Nav></Nav>
+    <div class="w">
+      <carousel></carousel>
+    </div>
   </div>
 </template>
 
 <script>
 import logo from './components/logo.vue'
 import Nav from './components/Nav.vue'
+import carousel from './components/carousel.vue'
 
 export default {
   name: 'App',
   components: {
     logo,
     Nav,
+    carousel,
   }
 }
 </script>
---
+++ src/components/carousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="car">
+    <el-carousel indicator-position="outside" height="385px" arrow="nerver">
+      <el-carousel-item v-for="(item,index) in carUrl" :key="index">
+        <img :src="item.imgUrl" alt="" width="100%" height="100%"/>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carUrl: [
+        { imgUrl: require("../assets/images/1.jpg") },
+        { imgUrl: require("../assets/images/4.jpg") },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* 引入初始化文件 */
+@import url("../assets/CSS/init.css");
+.car {
+  width: 927px;
+  height: 385px;
+  margin-top: 3px;
+}
+</style>
