d957a26692fe0caeab581b1ce3969a502a36d621
wensheng <1733115718@qq.com>
2020-12-27 15:05:13 +0800

    add picmenu

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@
       <carousel></carousel>
       <fast></fast>
     </div>
+    <picmenu></picmenu>
+    <div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
   </div>
 </template>
 
@@ -14,6 +26,7 @@ import logo from './components/logo.vue'
 import Nav from './components/Nav.vue'
 import carousel from './components/carousel.vue'
 import fast from './components/fast.vue'
+import picmenu from './components/picmenu.vue'
 
 export default {
   name: 'App',
@@ -22,6 +35,7 @@ export default {
     Nav,
     carousel,
     fast,
+    picmenu,
   }
 }
 </script>
---
+++ src/components/picmenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="picmenu w">
+    <div class="pic" v-for="item in picData" :key="item.id">
+      <a href="#">
+        <img :src="item.url" alt="">
+      </a>
+      <a href="#" class="name">{{item.name}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      picData: [
+        {
+          id: 1,
+          path: '',
+          name: '招生咨询',
+          url: require('../assets/images/pic01.jpg'),
+        },
+        {
+          id: 2,
+          path: '',
+          name: '招生咨询',
+          url: require('../assets/images/pic01.jpg'),
+        },
+        {
+          id: 3,
+          path: '',
+          name: '招生咨询',
+          url: require('../assets/images/pic01.jpg'),
+        },
+        {
+          id: 4,
+          path: '',
+          name: '招生咨询',
+          url: require('../assets/images/pic01.jpg'),
+        },
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+/* 引入初始化样式文件 */
+@import url("../assets/CSS/init.css");
+
+/* 给包裹住pic的盒子变成弹性布局 */
+.picmenu {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 30px;
+}
+.picmenu .name {
+  display: block;
+  width: 100%;
+  text-align: center;
+  height: 30px;
+  line-height: 30px;
+}
+.picmenu .name:hover {
+  border-bottom: 2px solid #be2634;
+}
+</style>
